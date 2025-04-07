d7fa2404d7c798fe724b6984e50761e7d532b9b7
森井成美 <narumi@moriinaruminoMacBook-Air.local>
2025-04-07 23:01:58 +0900

    delete

--- src/app/news/create/page.tsx
+++ src/app/news/create/page.tsx
@@ -1,17 +1,97 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { title } from "process";
+import { FormEvent, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+interface NewsForm {
+  title: string;
+  body: string;
+  userId: number;
+}
+const formSchema = z.object({
+  title: z.string().min(1, "タイトルは必須です"),
+  body: z.string().min(1, "内容は必須です"),
+  userId: z.number(),
+});
 
 export default function NewsCreate() {
+  const form = useForm<NewsForm>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { title: "", body: "", userId: 1 },
+  });
+  const router = useRouter();
+  const onSubmit = async (data: NewsForm) => {
+    console.log(data);
+    try {
+      const response = await axios.post("http://localhost:8000/news", data);
+      console.log(response);
+      router.push("/news");
+    } catch (error) {
+      alert("エラーが起きました");
+      console.log(error);
+    }
+  };
   return (
-    <div>
-      <h1>create</h1>
-      <Link href="/news">一覧に戻る</Link>
-      <form>
-        <div>
-          <input type="text" className="border-2"/>
-        </div>
-      </form>
-    </div>
+    <Card className="w-96 mx-auto p-7 mt-6">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div>
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>タイトル</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+
+            <FormField
+              control={form.control}
+              name="body"
+              render={({ field }) => {
+                return (
+                  <FormItem className="mt-6">
+                    <FormLabel>内容</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+          </div>
+
+          <Button className="mt-6">送信</Button>
+        </form>
+      </Form>
+      <Button asChild>
+        <Link href="/news">一覧に戻る</Link>
+      </Button>
+    </Card>
   );
 }
--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,42 +1,123 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import axios from "axios";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
+interface News {
+  id: number;
+  title: string;
+  body: string;
+  status: number;
+  userId: number;
+}
+
 export default function News() {
   const [loading, setLoading] = useState(true);
 
-  const [newsData, setNewsData] = useState([]);
+  const [newsData, setNewsData] = useState<News[]>([]);
+
+  const deleteNews = async (id: number) => {
+    const response = await axios.delete("http://localhost:8000/news/delete", {
+      data: { id: id },
+    });
+    fetch();
+  };
 
   const fetch = async () => {
-    const news = await axios.get("http://localhost:8000/news", {
-      withCredentials: true,
-    });
-    console.log(news.data);
+    setLoading(true);
+    const news = await axios.get("http://localhost:8000/news");
     setNewsData(news.data);
     setLoading(false);
-  };
+  }; 
 
   useEffect(() => {
     fetch();
   }, []);
   return (
-    <div>
-      <Link href="/news/create">
-        <div>新規作成</div>
-      </Link>
+    <div className="flex flex-row justify-center">
       {loading ? (
         <h2>Loading...</h2>
       ) : (
         <div>
-          {newsData.map((news, index) => {
-            return (
-              <div key={index}>
-                <div>タイトル:{news.title}</div>
-                <div>内容:{news.body}</div>
-              </div>
-            );
-          })}
+          <div className="flex justify-end items-center mb-4">
+            <Button>
+              <Link href="/news/create">
+                <div>新規作成</div>
+              </Link>
+            </Button>
+          </div>
+          <Table className="w-24">
+            <TableHeader>
+              <TableRow>
+                <TableHead className="max-w-32 font-bold">タイトル</TableHead>
+                <TableHead className="max-w-48 font-bold">内容</TableHead>
+                <TableHead className="max-w-48 font-bold"></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {newsData.map((news, index) => (
+                <TableRow key={index}>
+                  <TableCell className="whitespace-nowrap overflow-hidden text-ellipsis max-w-[200px]">
+                    {news.title}
+                  </TableCell>
+                  <TableCell className="whitespace-nowrap overflow-hidden text-ellipsis max-w-[200px]">
+                    {news.body}
+                  </TableCell>
+                  <TableCell className="whitespace-nowrap overflow-hidden text-ellipsis max-w-[200px]">
+                    <Dialog>
+                      <DialogTrigger asChild>
+                        <Button>削除</Button>
+                      </DialogTrigger>
+                      <DialogContent className="sm:max-w-[425px]">
+                        <DialogHeader>
+                          <DialogTitle>
+                            本当に削除してもよいですか？
+                          </DialogTitle>
+                        </DialogHeader>
+
+                        <DialogFooter>
+                          <DialogClose asChild>
+                            <Button
+                              type="submit"
+                              onClick={() => deleteNews(news.id)}
+                            >
+                              はい
+                            </Button>
+                          </DialogClose>
+                          <DialogClose asChild>
+                            <Button type="button" variant="secondary">
+                              閉じる
+                            </Button>
+                          </DialogClose>
+                        </DialogFooter>
+                      </DialogContent>
+                    </Dialog>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </div>
       )}
     </div>
